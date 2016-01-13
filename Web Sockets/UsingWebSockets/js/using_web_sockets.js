e133e654dd653c5672d794634e62a2ae116a43c1
Shaun Lewis <lewis.shaun1@outlook.com>
2016-01-12 21:49:51 -0500

    commit

---
+++ Web Sockets/UsingWebSockets/js/using_web_sockets.js
@@ -0,0 +1,34 @@
+﻿var create_socket = function () {
+  
+    //check for browser support
+    if (window.WebSocket) {
+        alert("WebSocket Status: Supported");
+    }
+    else {
+        alert("WebSocket Status: Not supported");
+    }
+
+    //create new WebSocket instance
+    var webSoc = new WebSocket("ws://localhost:52263/WebSocketsServer.ashx"); //wss -> web socket secure
+
+
+    //set status (method 1)
+    webSoc.onopen = function () {
+        alert("Connection Status: Open");
+    }
+    
+    
+}
+
+create_socket();
+
+
+/*
+Check if broswer support web socket
+
+
+if (window.WebSocket)
+    alert("WebSocket Status: Supported");
+else
+    alert("WebSocket Status: Not Supported");
+*/
