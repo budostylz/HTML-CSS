9f2f999390555d8a4fad1f21f0f0784f5c8218fb
Shaun Lewis <lewis.shaun1@outlook.com>
2016-01-17 14:09:33 -0500

    commit

---
+++ Web Workers/WorkMe/js/using_web_workers.js
@@ -0,0 +1,39 @@
+﻿
+$(document).ready(function () {
+    var w;
+    
+    var start_worker = $("#start_worker").click(function () {
+
+        if (typeof (Worker) !== "undefined") {//check browser support 
+            if (typeof (w) == "undefined") {
+                w = new Worker("/js/workerCode.js");//create worker
+            }
+            w.onmessage = function (event) {
+                $("#result").text( event.data );//get response from worker
+            };
+        } else {
+            $("#result").text("Sorry! No Web Worker support.");
+        }
+
+    });
+
+    var stop_worker = $("#stop_worker").click(function () {
+        alert("false");
+
+    });
+});
+
+
+
+
+
+
+    
+    
+    
+
+
+function stopWorker() {
+    w.terminate();
+    w = undefined;
+}
