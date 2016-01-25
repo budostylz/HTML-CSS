b2ba5d706a3da5ef3669f9c14d28958494471689
Shaun Lewis <lewis.shaun1@outlook.com>
2016-01-24 20:19:10 -0500

    commit

---
+++ HTML5 APIs/drag-and-drop/README.md
@@ -0,0 +1,18 @@
+##Drag and Drop (w3Schools)
+
+http://www.w3schools.com/HTML/html5_draganddrop.asp
+
+
+
+
+
+
+
+
+
+
+	
+	
+
+
+
---
+++ HTML5 APIs/drag-and-drop/drag-and-drop.js
@@ -0,0 +1,15 @@
+﻿
+    var allowDropping = function (e) {
+        e.preventDefault();
+        
+    }
+
+    var dragging = function (e) {
+        e.dataTransfer.setData("Text", event.target.id);
+    }
+
+    var dropping = function (e) {
+        e.preventDefault();
+        var info = e.dataTransfer.getData("Text");
+        e.target.appendChild(document.getElementById(info));
+    }
